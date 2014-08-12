f1b07b8aada7ed758f679686036c48711c3d0759
Morgan Phillips <morgan@artistgrowth.com>
2014-08-12 01:23:38 -0500

    Fixes jenkins role.

--- deployment/ansible-vagrant.sh
+++ deployment/ansible-vagrant.sh
@@ -1,2 +1,2 @@
 export ANSIBLE_HOST_KEY_CHECKING=False;
-/usr/bin/env ansible-playbook -i vagrant --private-key=/Applications/Vagrant/embedded/gems/gems/vagrant-1.6.3/keys/vagrant $@ --ask-vault-pass
+/usr/bin/env ansible-playbook -i vagrant --private-key=/Applications/Vagrant/embedded/gems/gems/vagrant-1.6.3/keys/vagrant "$@"
