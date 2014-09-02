d8c3c077676b065b1beab1bc313edf28e055d6fc
mrrrgn <winter2718@gmail.com>
2014-09-02 01:51:33 +0000

    Adds docker support.

--- deployment/ansible-vagrant.sh
+++
@@ -1,2 +0,0 @@
-export ANSIBLE_HOST_KEY_CHECKING=False;
-/usr/bin/env ansible-playbook -i vagrant --private-key=/Applications/Vagrant/embedded/gems/gems/vagrant-1.6.3/keys/vagrant "$@"
--- deployment/deploy-local.sh
+++
@@ -1,1 +0,0 @@
-ansible-playbook webservers.yml -i local "$@" -c local
