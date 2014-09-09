055a0f91d98672e3fa11c7398c74675b261f8ea7
Morgan Phillips <morgan@artistgrowth.com>
2014-09-09 12:20:36 -0500

    Revert "Revert "Bring back site.""
    This reverts commit 044f50edf5061e184e8dfafd366ca1293ec8c743.

--- poetry/urls.py
+++ poetry/urls.py
@@ -1,22 +1,16 @@
 import os
 
 from django.conf.urls import patterns, include, url
-#import linuxpoetry.urls
+import linuxpoetry.urls
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Sorry, linux-poetry.com is down for the time being.  - winter2718@gmail.com")
 
 urlpatterns = patterns(
     '',
-    url(r'^', index),
-    #url(r'^', include(linuxpoetry.urls)),
+    url(r'^', include(linuxpoetry.urls)),
 )
 
 if os.environ.get('POETRY_ADMIN') == '1':
